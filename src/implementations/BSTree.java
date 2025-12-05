36ed8c3e2ac8c09c7f70eba8f655a1b25299337f
Penncilk <Jude.Uyeno@edu.sait.ca>
2025-12-04 19:21:26 -0700

    MOST things work in BST implimentation

--- src/implementations/BSTree.java
+++ src/implementations/BSTree.java
@@ -2,5 +2,158 @@ package implementations;
 
 public class BSTree<E extends Comparable<? super E>>
 {
+	private BSTreeNode<E> head;
+	
+	public BSTree() {
+		head = null;
+	}
+	
+	public BSTreeNode<E> getRoot() throws NullPointerException {
+		if (head == null) throw new NullPointerException();
+		return head;
+	}
+	
+	// Recursive way to grab height
+	private int heightGrab(int height, BSTreeNode<E> node) {
+		if (node == null) return height;
+		int newheight = height + 1;
+		
+		int leftheight = heightGrab(newheight, node.getLeft());
+		int rightheight = heightGrab(newheight, node.getRight());
+		
+		if (leftheight > rightheight) return leftheight;
+		else return rightheight;
+	}
+	
+	public int getHeight() {
+		return heightGrab(0, head);
+	}
+	
+	// Recursive way to get size
+	private int sizeGrab(BSTreeNode<E> node) {
+		if (node == null)
+            return 0;
 
+        int left = sizeGrab(node.getLeft());
+        int right = sizeGrab(node.getRight());
+
+        return left + right + 1;
+	}
+	
+	public int size() {
+		if (head == null) return 0;
+		return sizeGrab(head);
+	}
+	
+	public boolean isEmpty() {
+		return (head == null);
+	}
+	
+	public void clear() {
+		head = null;
+	}
+	
+	// Recursive way to find item
+	private boolean checkContains(E entry, BSTreeNode<E> node) {
+		if (node == null) return false;
+		if (node.getElement() == entry) return true;
+		return checkContains(entry, node.getLeft()) || checkContains(entry, node.getRight());
+	}
+	
+	public boolean contains( E entry ) throws NullPointerException {
+		if (entry == null) throw new NullPointerException();
+		return checkContains(entry, head);
+	}
+	
+	// Recursive way to retrive item
+	private BSTreeNode<E> checkSearch(E entry, BSTreeNode<E> node) {
+		if (node == null) return null;
+		if (node.getElement() == entry) return node;
+		
+		BSTreeNode<E> leftsearch = checkSearch(entry, node.getLeft());
+		BSTreeNode<E> rightsearch = checkSearch(entry, node.getRight());
+		if (leftsearch != null) return leftsearch;
+		if (rightsearch != null) return rightsearch;
+		return null;
+	}
+	
+	public BSTreeNode<E> search( E entry ) throws NullPointerException {
+		if (entry == null) throw new NullPointerException();
+		return checkSearch(entry, head);
+	}
+	
+	private boolean addLogic( E newEntry, BSTreeNode<E> node ) {
+		int compare = newEntry.compareTo(node.getElement());
+		if (compare < 0) {
+			if (node.getLeft() == null) {
+				BSTreeNode<E> newNode = new BSTreeNode<E>();
+				newNode.setElement(newEntry);
+				node.setLeft(newNode);
+				return true;
+			} else {
+				return addLogic(newEntry, node.getLeft());
+			}
+		}
+		if (compare > 0) {
+			if (node.getRight() == null) {
+				BSTreeNode<E> newNode = new BSTreeNode<E>();
+				newNode.setElement(newEntry);
+				node.setRight(newNode);
+				return true;
+			} else {
+				return addLogic(newEntry, node.getRight());
+			}
+		}
+		return false;
+	}
+	
+	public boolean add( E newEntry ) throws NullPointerException {
+		if (newEntry == null) throw new NullPointerException();
+		if (head == null) {
+			head = new BSTreeNode<E>();
+			head.setElement(newEntry);
+		}
+		return addLogic(newEntry, head);
+	}
+	
+	public BSTreeNode<E> removeMin() {
+		BSTreeNode<E> node = head;
+		BSTreeNode<E> returnNode;
+		if (node == null) return null;
+		if (getHeight() == 1) {
+			returnNode = head;
+			head = null;
+			return returnNode;
+		}
+		
+		while (true) {
+			if (node.getLeft().getLeft() == null) {
+				returnNode = node.getLeft().getLeft();
+				node.setLeft(null);
+				return returnNode;
+			}
+			node = node.getLeft();
+		}
+	}
+	
+	public BSTreeNode<E> removeMax() {
+		BSTreeNode<E> node = head;
+		BSTreeNode<E> returnNode;
+		if (node == null) return null;
+		if (getHeight() == 1) {
+			returnNode = head;
+			head = null;
+			return returnNode;
+		}
+		
+		while (true) {
+			if (node.getRight().getRight() == null) {
+				returnNode = node.getRight().getRight();
+				node.setRight(null);
+				return returnNode;
+			}
+			node = node.getRight();
+		}
+	}
+	
 }
--- src/implementations/BSTreeNode.java
+++ src/implementations/BSTreeNode.java
@@ -2,5 +2,35 @@ package implementations;
 
 public class BSTreeNode<E>
 {
-
+	private E Element;
+	private BSTreeNode<E> Left;
+	private BSTreeNode<E> Right;
+	
+	public BSTreeNode() {
+		Element = null;
+		Left = null;
+		Right = null;
+	}
+	
+	// G&S for Data
+	public E getElement() {
+		return Element;
+	}
+	public void setElement(E val) {
+		Element = val;
+	}
+	// G&S for Left Node
+	public BSTreeNode<E> getLeft() {
+		return Left;
+	}
+	public void setLeft(BSTreeNode<E> node) {
+		Left = node;
+	}
+	// G&S for Right Node
+	public BSTreeNode<E> getRight() {
+		return Right;
+	}
+	public void setRight(BSTreeNode<E> node) {
+		Right = node;
+	}
 }
