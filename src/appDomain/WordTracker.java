5da61d4891498313b9e50d3ac5de5ef1854639df
NoahStanich <noah.stanich@edu.sait.ca>
2025-12-09 23:16:36 -0700

    WordTracker commit

--- src/appDomain/WordTracker.java
+++ src/appDomain/WordTracker.java
@@ -9,32 +9,45 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Iterator;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import implementations.BSTree;
-import implementations.BSTreeNode;
 
 public class WordTracker {
-	public static void main(String[] args) {
-		File fileName = new File("res/test1.txt");
+	public static void main(String[] args) throws ClassNotFoundException {
+		File file = new File("res/test1.txt");
 		BSTree<String> tree = new BSTree<String>();
-		BSTreeNode<String> treeNode = new BSTreeNode<String>();
+		ArrayList<String> treeList = new ArrayList<String>();
+		Map<Integer, Integer> lineNumbers = new HashMap<>();
+		Map<String, Integer> entries = new HashMap<>();
 		Integer lineNumber = 0;
 		Boolean repExists = false;
-		Scanner scanner = new Scanner(System.in);
-		String option = scanner.nextLine();
-		scanner.close();
+		String option = "po";
 		
-		try (Scanner fileReader = new Scanner(fileName)) {
+		try (Scanner lineReader = new Scanner(file)) {
+	        while (lineReader.hasNextLine()) {
+	            lineReader.nextLine();
+	            lineNumber++;
+	        }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try (Scanner fileReader = new Scanner(file)) {
 			while (fileReader.hasNextLine()) {
-				//if (fileReader.next().contains(",") || fileReader.next().contains(".") || fileReader.next().contains("!")) {
-				//	tree.add(fileReader.next());
-				//}
 				tree.add(fileReader.next().toLowerCase());
+				treeList.add(fileReader.next().toLowerCase());
+				for (String word : treeList) {
+					entries.put(word, entries.getOrDefault(word, 0) + 1);
+				}
 			}
 			
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("res/repository.ser"));
 			for(int i = 0; i < tree.size(); i++)
 			{
-				oos.writeObject(tree);
+				oos.writeObject(treeList);
 			}
 			repExists = true;
 			oos.close();
@@ -49,7 +62,9 @@ public class WordTracker {
 			try
 			{
 				ObjectInputStream ois = new ObjectInputStream(new FileInputStream("res/repository.ser"));
-				tree = (BSTree<String>) ois.readObject();
+				for (String word : treeList) {
+					tree.add(word);
+				}
 				ois.close();
 			}
 			catch (FileNotFoundException e)
@@ -60,30 +75,28 @@ public class WordTracker {
 			{
 				e.printStackTrace();
 			}
-			catch (ClassNotFoundException e)
-			{
-				e.printStackTrace();
-			}
 		}
 		
-		Iterator<String> iterator = (Iterator<String>) tree.inorderIterator();
+		Iterator<String> iterator = tree.inorderIterator();
 		
 		switch(option) {
 			case "pf":
 				System.out.println("Displaying -pf format");
 				while (iterator.hasNext()) {
-					System.out.println("Key : ===" + iterator.next() + "===" + " found in file: " + fileName);
+					System.out.println("Key : ===" + iterator.next().replaceAll("\\p{Punct}", "") + "===" + " found in file: " + file);
 				}
 				break;
 			case "pl":
 				System.out.println("Displaying -pl format");
-				for (int i = 0; i < tree.size(); i++) {
-					System.out.println("Key : ===" + treeNode.getElement() + "===" + " found in file: " + fileName + " on lines: " + lineNumber);
+				while(iterator.hasNext()) {
+					System.out.println("Key : ===" + iterator.next().replaceAll("\\p{Punct}", "") + "===" + " found in file: " + file + " on lines: " + lineNumbers.get(lineNumber));
 				}
 				break;
 			case "po":
 				System.out.println("Displaying -po format");
-				System.out.println("Key : ===" + tree.inorderIterator() + "=== number of entries: " + " found in file: " + fileName + "on lines: " + lineNumber);
+				while (iterator.hasNext()) {
+					System.out.println("Key : ===" + iterator.next().replaceAll("\\p{Punct}", "") + "=== number of entries: " + entries.keySet() + " found in file: " + file + " on lines: " + lineNumber);
+				}
 				break;
 			default:
 				System.out.println("Invalid option.");
